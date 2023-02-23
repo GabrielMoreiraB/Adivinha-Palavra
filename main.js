39285dd7fea2d60885807951d4e8966678c32e09
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2023-02-23 16:58:39 -0300

    adesão de classes ok

--- main.js
+++ main.js
@@ -11,21 +11,34 @@ const tecl1 = document.getElementById('teclado-linha1');
 const tecl2 = document.getElementById('teclado-linha2');
 const tecl3 = document.getElementById('teclado-linha3');
 
+const rows = 6;
+let linhaAtual = 0;
+const columns = 5;
+let colunaAtual = 0;
+const palavra = 'VASCO';
+let palpite = [];
+let mapaPalavra = [];
+
+for (let i = 0; i < palavra.length; i++) {
+    mapaPalavra[palavra[i]] = i;
+}
+//console.log(mapaPalavra)
+//console.log(mapaPalavra['p'])
+
 const conteudo1 = ['Q' , 'W' , 'E' , 'R' , 'T' , 'Y' , 'U' , 'I' , 'O' , 'P'];
 const conteudo2 = ['A' , 'S' , 'D' , 'F' , 'G' , 'H' , 'J' , 'K' , 'L'];
 const conteudo3 = ['Z' , 'X' , 'C' , 'V' , 'B' , 'N' , 'M'];
 
-const row = 6;
-const column = 5;
 
-for(let i=0; i<row; i++ ){
+
+for(let i=0; i<rows; i++ ){
     const tetativaLinha = document.createElement('div');
     tetativaLinha.setAttribute('id', i);
     tetativaLinha.setAttribute('class', 'chute-linha');
-    for(let x = 0; x < column ; x++){
+    for(let x = 0; x < columns ; x++){
         const tetativaColuna = document.createElement('div');
         tetativaColuna.setAttribute('id','linha'+ i + 'coluna' + x);
-        tetativaColuna.setAttribute('class', 'chute-coluna');
+        tetativaColuna.setAttribute('class', i === 0 ?  'chute-coluna focus' : 'chute-coluna');
         tetativaLinha.appendChild(tetativaColuna);
     }
     tentativas.appendChild(tetativaLinha)
@@ -39,10 +52,73 @@ array.forEach(function(key){
     btn.setAttribute('id', key);
     btn.setAttribute('class', 'tecla');
     btn.addEventListener('click', function(){
-        console.log('tecla: ', key)
+        addLetraItem(key)
     })
     pai.appendChild(btn)
 })
+}
+
+function addLetraItem(key){
+    if(colunaAtual == columns) return
+    const chuteatual = document.querySelector('#linha'+linhaAtual+'coluna'+colunaAtual);
+    chuteatual.textContent = key;
+    colunaAtual++;
+    palpite.push(key);
+    //console.log(palpite)
+}
+
+
+function checkPalpite (){
+    if(palpite.length !== columns) return
+
+    palpite = palpite.join("");
+
+    let colunasDePalpites = document.querySelectorAll('.focus');
+    //console.log(colunasDePalpites)
+
+    for(let i = 0; i < columns; i++){
+        const letraTest = palpite[i];
+        
+        if(mapaPalavra[letraTest] === undefined){
+
+            colunasDePalpites[i].classList.add('errado')
+        } else {
+            if(mapaPalavra[letraTest] === i){
+                colunasDePalpites[i].classList.add('certo')
+            } else{
+                colunasDePalpites[i].classList.add('talvez')
+            }
+        }
+    }
+
+    if(palpite === palavra){
+        window.alert('Acertou!')
+    }
+    if(linhaAtual === rows-1){
+        window.alert('Errou!')
+    }else{
+        moveProxLinha();
+        
+    }
+}
+
+function moveProxLinha(){
+    let colunasDePalpites = document.querySelectorAll('.focus');
+    colunasDePalpites.forEach(function(item){
+        item.classList.remove('focus')
+    })
+    colunaAtual = 0;
+    linhaAtual++;
+    palpite = [];
+
+    let linhaAlvo = document.getElementById(linhaAtual);
+    //console.log(linhaAlvo)
+    let colunasAlvo = linhaAlvo.querySelectorAll('.chute-coluna');
+    //console.log(colunasAlvo)
+    colunasAlvo.forEach(function(item){
+        item.classList.add('focus');
+    })
+
 }
 
 preencheTeclado (conteudo1, tecl1);
@@ -62,7 +138,5 @@ const enter = document.createElement('button')
 enter.textContent = 'ENTER'
 enter.setAttribute('id', 'enter');
 enter.setAttribute('class', 'tecla');
-enter.addEventListener('click', () => {
-    console.log('enter')
-})
+enter.addEventListener('click', checkPalpite)
 teclApaga.appendChild(enter)
